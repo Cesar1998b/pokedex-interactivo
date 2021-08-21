af9adc1ba77fe2b581776fdb6dbf4d8c7729e6bb
Cesar David Botina Chacon <cdbc9082@coomeva.nal>
2021-08-20 19:16:37 -0500

    add pokeapi service with pokemon services

---
+++ src/app/services/pokemon.services.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+
+
+@Injectable({providedIn: 'root'})
+export class PokemonService{
+  private baseUrl = `https://pokeapi.co/api/v2`;
+
+  constructor(private http: HttpClient){ }
+
+
+  getPokemonList(url){
+    return this.http.get(url);
+  }
+
+  getPokemonInfo(name: string){
+    return this.http.get(this.baseUrl + `/pokemon/${name}`);
+  }
+
+}
